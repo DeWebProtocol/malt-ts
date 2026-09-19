3b76dec72b988238f28867b94c6f0aaa03d41e1c
Ubuntu <vitalikzhou@gmail.com>
2026-09-19 09:57:01 +0800

    release: bind browser assets to Core v0.0.9-rc.2

--- src/release.mjs
+++ src/release.mjs
@@ -1,9 +1,9 @@
-export const maltTSVersion = '0.0.2-RC.1'
+export const maltTSVersion = '0.0.2-rc.2'
 
 export const maltCoreRelease = Object.freeze({
-  version: 'v0.0.9-RC.1',
-  commit: '5fb528ba82f69f9a0acc3df5affb74181c61c827',
-  manifestSHA256: '90347e262307c18dcf7a271c995dc105ce9f5389f6d86ec0a24cb35dc0ddd533',
-  verifierAssetSetSHA256: 'ac0130a36a488668a89c0e3175d44069a8e937ee9fbc3da8b46f7626bed02db4',
-  writerAssetSetSHA256: '6f845c07588fc302fcd59480544e04933b22689e4f31ec0afd325e1d808cf128'
+  version: 'v0.0.9-rc.2',
+  commit: 'ccae498e29a323eb003b74fb12aa23b87bd13832',
+  manifestSHA256: '6fe0b21435beea89a5a2f2d90668312ed43db5050a5f66ccc34e65c135ab6b1a',
+  verifierAssetSetSHA256: 'c0908a8080970c1834f32d37c6fa07bb46112afff1834f09ba326082a05db95c',
+  writerAssetSetSHA256: 'ab30b8637dcacc81ad63ea716b68506694bc9fe216b3090c92b5211b170b9e1f'
 })
--- types/release.d.ts
+++ types/release.d.ts
@@ -1,8 +1,8 @@
-export declare const maltTSVersion: '0.0.2-RC.1'
+export declare const maltTSVersion: '0.0.2-rc.2'
 
 export interface MaltCoreRelease {
-  readonly version: 'v0.0.9-RC.1'
-  readonly commit: '5fb528ba82f69f9a0acc3df5affb74181c61c827'
+  readonly version: 'v0.0.9-rc.2'
+  readonly commit: 'ccae498e29a323eb003b74fb12aa23b87bd13832'
   readonly manifestSHA256: string
   readonly verifierAssetSetSHA256: string
   readonly writerAssetSetSHA256: string
