02cee22e4f0ceae2aaf4941af16de31f3e592779
Ubuntu <vitalikzhou@gmail.com>
2026-09-21 15:38:24 +0800

    release: bind malt-ts rc.5 to Core v0.0.9-rc.6

--- src/release.mjs
+++ src/release.mjs
@@ -1,8 +1,8 @@
 export const maltTSVersion = '0.0.2-rc.5'
 
 export const maltCoreRelease = Object.freeze({
-  version: 'v0.0.9-rc.5',
-  commit: '8a04f060d307d6b3ffec2d8b3fafb057eb9ac653',
-  moduleSum: 'h1:guVYvNlbHiQn2i9qk0ZAm4zdZZUcDd8Q3THEh2qpoeQ=',
+  version: 'v0.0.9-rc.6',
+  commit: '9f1794e15df0414c95c217b3526d6b57631c1193',
+  moduleSum: 'h1:wRuE+qrKghWFWWzXL3Gkn6qdByZeTgUL8JgEPf/4Ld4=',
   goModSum: 'h1:o6L8K9vJp5hyrVo3fUtSsj0rvNpcboOcOb3nQwnR/gs='
 })
